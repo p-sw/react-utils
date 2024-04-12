0f5e66e37be4684acd7d45da95b5246dc22a3a5c
p-sw <shinwoo.park@psw.kr>
2024-04-12 14:44:03 +0900

    fix: make isPromise any parameter

--- src/utils/isPromise.ts
+++ src/utils/isPromise.ts
@@ -1,4 +1,4 @@
-export function isPromise(f: (...p: any[]) => any): boolean {
+export function isPromise(f: any): boolean {
   return !!(f.constructor.name === 'AsyncFunction' || (typeof f === 'object' && (<{
     then?: () => void
   }>f)["then"] && typeof (<{ then?: () => void }>f)["then"] === 'function'))
