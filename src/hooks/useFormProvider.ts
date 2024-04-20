dbe4db3c578165b15e1a9024940b3b0a2b009629
p-sw <shinwoo.park@psw.kr>
2024-04-20 21:46:18 +0900

    feat(useFormProvider): use useActionState to get return value of action

--- src/hooks/useFormProvider.ts
+++ src/hooks/useFormProvider.ts
@@ -1,13 +1,19 @@
 import { FormStatus } from "react-dom";
-import { Dispatch, SetStateAction, useState } from "react";
+import { Dispatch, SetStateAction, useState, useActionState } from "react";
 
 export function useFormProvider(): [FormStatus, Dispatch<SetStateAction<FormStatus>>];
-export function useFormProvider<AP, AR>(action: (e: AP) => Promise<AR>): [FormStatus, Dispatch<SetStateAction<FormStatus>>, (e: FormData) => Promise<AR>];
-export function useFormProvider<AP, AR>(action?: (e: AP) => Promise<AR>): [FormStatus, Dispatch<SetStateAction<FormStatus>>, ((e: FormData) => Promise<AR>)?] {
+export function useFormProvider<AP, AR>(action: (e: AP, p: AR | null) => Promise<AR>): [FormStatus, Dispatch<SetStateAction<FormStatus>>, (e: FormData) => void, AR | null];
+export function useFormProvider<AP, AR>(action?: (e: AP, p: AR | null) => Promise<AR>): [FormStatus, Dispatch<SetStateAction<FormStatus>>, ((e: FormData) => void)?, (AR | null)?] {
   const [formStatus, setFormStatus] = useState<FormStatus>({ pending: false, data: null, action: null, method: null });
-  const submitFunction = action ? async (e: FormData) => {
-    return action(Object.fromEntries(e) as AP);
-  } : undefined;
+  const [state, formAction] = useActionState<AR | null, FormData>(async (prev, form) => {
+    const n = Object.fromEntries(form) as AP;
+    return action?.(n, prev) ?? null;
+  }, null)
 
-  return [formStatus, setFormStatus, submitFunction];
+  if (action) {
+    return [formStatus, setFormStatus, formAction, state];
+  } else {
+    return [formStatus, setFormStatus];
+  }
 }
+
