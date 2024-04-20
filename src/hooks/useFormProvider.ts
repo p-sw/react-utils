c9466395e86eb1c83658056fa81edf6cdb7367da
p-sw <shinwoo.park@psw.kr>
2024-04-20 21:12:11 +0900

    feat(useFormProvider): add action parameter

--- src/hooks/useFormProvider.ts
+++ src/hooks/useFormProvider.ts
@@ -1,6 +1,13 @@
 import { FormStatus } from "react-dom";
 import { Dispatch, SetStateAction, useState } from "react";
 
-export function useFormProvider(): [FormStatus, Dispatch<SetStateAction<FormStatus>>] {
-  return useState<FormStatus>({ pending: false, data: null, action: null, method: null });
+export function useFormProvider(): [FormStatus, Dispatch<SetStateAction<FormStatus>>];
+export function useFormProvider<AP, AR>(action: (e: AP) => Promise<AR>): [FormStatus, Dispatch<SetStateAction<FormStatus>>, (e: FormData) => Promise<AR>];
+export function useFormProvider<AP, AR>(action?: (e: AP) => Promise<AR>): [FormStatus, Dispatch<SetStateAction<FormStatus>>, ((e: FormData) => Promise<AR>)?] {
+  const [formStatus, setFormStatus] = useState<FormStatus>({ pending: false, data: null, action: null, method: null });
+  const submitFunction = action ? async (e: FormData) => {
+    return action(Object.fromEntries(e) as AP);
+  } : undefined;
+
+  return [formStatus, setFormStatus, submitFunction];
 }
